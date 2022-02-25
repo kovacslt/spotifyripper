c2f8254a8eaa29594d4621daaf5fef9c427595ec
kovacslt <info@kovacsoltvideo.hu>
2022-02-25 16:02:27 +0100

    Update notify.sh
    Also get track number from notification.
    Don't emit "__SWITCH__" too frequently, there is now  at least 10 seconds between them - this helps to keep the start of a song more clean, where Spotify client emits track changes in a burst

--- notify.sh
+++ notify.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
 metadata=0
-variant=""
+variant="" 
+lasttime=$(date +"%s");
 dbus-monitor "path=/org/mpris/MediaPlayer2,member=PropertiesChanged"|while read line
 do
+
   col=$(echo "$line" | awk -F '"' '{print $2}')
+  col2=$(echo "$line" | awk -F ' ' '{print $3}')
+
   if [[ "$col" == "org.mpris.MediaPlayer2.Player" ]]; then
-    metadata=1
-    variant=""
-    echo "__SWITCH__"
-  elif (($metadata)); then
-    if [[ -n $(echo "$line"|grep "dict entry") ]]; then
+    curtime=$(date +"%s")
+    deltat=$(($curtime - $lasttime))
+    if [[ "$deltat" -ge 10 ]]; then 
+      metadata=1
       variant=""
-    elif [[ -n $variant ]] && [[ $variant != 0 ]]; then
+      echo "__SWITCH__"
+     lasttime=$curtime
+    fi
+      elif (($metadata)); then
+        if [[ -n $(echo "$line"|grep "dict entry") ]]; then
+        variant=""
+      elif [[ -n $variant ]] && [[ $variant != 0 ]]; then
       if [[ -n $col ]]; then
         simplevariant=$(echo "$variant" | cut -d: -f2)
         echo "$simplevariant=$col"
         variant=0
       fi
+      if [[ -n $col2 ]] ; then
+        simplevariant2=$(echo "$variant" | cut -d: -f2)
+        if [[ $simplevariant2 == "trackNumber" ]]; then
+            echo "$simplevariant2=$col2"
+        fi
+      fi 
+
     elif [[ -n $col ]]; then
       variant="$col"
-      # echo "variant = $col"
+     # echo "variant = $col"
     fi
   fi
 done
